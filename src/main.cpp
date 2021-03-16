e2e64b8c07716878a3271174492078eae3c643e2
noonbi <noonbi715@gmail.com>
2021-03-16 01:23:37 -0400

    file update

--- src/main.cpp
+++ src/main.cpp
@@ -1094,7 +1094,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 3 * 60 * 60;     // Poscoin: 3days
+static const int64 nTargetTimespan = 3 * 60 * 60;     // Poscoin: 3Hours
 static const int64 nTargetSpacing = 3 * 60;       // Poscoin: 3minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
